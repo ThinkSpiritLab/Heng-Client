dcceee4fae02240542d2b93aebf3044ab2d1e317
flaeyer <a@oi.al>
2021-09-07 17:01:19 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -25,16 +25,16 @@ dnf update --assumeyes && \
     screen
 npm install -g cnpm --registry=https://registry.npm.taobao.org
 cd ~
-nohup wget -O /usr/bin/ojcmp https://github.com.cnpmjs.org/ThinkSpiritLab/ojcmp/releases/download/v0.4.0/ojcmp-v0.4.0-x86_64-unknown-linux-gnu \
-    && chmod 755 /usr/bin/ojcmp &
+wget -O /usr/bin/ojcmp https://github.com.cnpmjs.org/ThinkSpiritLab/ojcmp/releases/download/v0.4.0/ojcmp-v0.4.0-x86_64-unknown-linux-gnu \
+    && chmod 755 /usr/bin/ojcmp
 cd ~
-nohup wget https://download.redis.io/releases/redis-6.2.5.tar.gz && tar -zxf redis-6.2.5.tar.gz && cd redis-6.2.5 && make && make install &
+wget https://download.redis.io/releases/redis-6.2.5.tar.gz && tar -zxf redis-6.2.5.tar.gz && cd redis-6.2.5 && make && make install
 cd ~
-nohup git clone https://github.com.cnpmjs.org/flaryer/Heng-Core.git && cd Heng-Core && make && make install &
+git clone https://github.com.cnpmjs.org/flaryer/Heng-Core.git && cd Heng-Core && make && make install
 cd ~
-nohup git clone https://github.com.cnpmjs.org/google/nsjail.git && cd nsjail &&
-    make && mv ./nsjail /usr/bin && rm -rf -- /nsjail &
+git clone https://github.com.cnpmjs.org/google/nsjail.git && cd nsjail &&
+    make && mv ./nsjail /usr/bin && rm -rf -- /nsjail
 cd ~
-nohup git clone https://github.com.cnpmjs.org/flaryer/Heng-Client.git && cd Heng-Client && git checkout dev && cnpm i && npm run build &
+git clone https://github.com.cnpmjs.org/flaryer/Heng-Client.git && cd Heng-Client && git checkout dev && cnpm i && npm run build
 cd ~
-nohup git clone https://github.com.cnpmjs.org/flaryer/heng-controller.git && cd heng-controller && git checkout dev && cnpm i && npm run build &
+git clone https://github.com.cnpmjs.org/flaryer/heng-controller.git && cd heng-controller && git checkout dev && cnpm i && npm run build
