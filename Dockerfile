86ba43e4c1a9cc369d76434a6e42dbf4f9001fd4
flaeyer <a@oi.al>
2021-10-27 20:31:43 +0800

    docker test

--- Dockerfile
+++ Dockerfile
@@ -1,54 +1,5 @@
 FROM centos:8
-RUN dnf update --assumeyes && \
-    # yum install --assumeyes dnf-plugins-core && \
-    # yum config-manager --set-enabled powertools && \
-    # dnf install --assumeyes epel-release && \
-    dnf module --assumeyes install nodejs:14 && \
-    npm install -g npm && \
-    dnf install --enablerepo=powertools --assumeyes \
-    autoconf \
-    bison \
-    flex \
-    gcc \
-    gcc-c++ \
-    libstdc++ \
-    libstdc++-static \
-    glibc-static \
-    git \
-    protobuf-devel \
-    libnl3-devel \
-    libtool \
-    make \
-    pkg-config \
-    protobuf-compiler \
-    git \
-    wget \
-    java-1.8.0-openjdk \
-    java-1.8.0-openjdk-devel \
-    python3 \
-    && dnf clean all \
-    && rm -rf /var/cache/yum \
-    && rm -rf /var/cache/dnf
-RUN wget -O /usr/bin/ojcmp https://github.com.cnpmjs.org/ThinkSpiritLab/ojcmp/releases/download/v0.4.0/ojcmp-v0.4.0-x86_64-unknown-linux-gnu \
-    && chmod 755 /usr/bin/ojcmp
-#WORKDIR /ojcmp
-#RUN git clone https://github.com/ThinkSpiritLab/ojcmp.git /ojcmp && \
-    #curl https://sh.rustup.rs -sSf | bash -s -- -y && \
-    #source $HOME/.cargo/env && \
-    #cargo build --release && \
-    #cp target/release/ojcmp /usr/bin && \
-    #RM -rf -- /ojcmp
-# WORKDIR /hc
-# RUN git clone https://github.com.cnpmjs.org/ThinkSpiritLab/Heng-Core.git /hc && \
-#     make && \
-#     make install && \
-#     rm -rf -- /hc
-WORKDIR /nsjail
-RUN git clone https://github.com.cnpmjs.org/flaryer/nsjail.git /nsjail && git checkout real_usr_time_kill \
-    make && mv /nsjail/nsjail /usr/bin && rm -rf -- /nsjail
-WORKDIR /usr/src/app
-# COPY package*.json ./
-RUN npm install
+WORKDIR /hc
 COPY . .
-RUN npm run build
+RUN bash ./prepare.sh
 CMD ["node", "dist/index.js"]
--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,7 @@
-dnf update --assumeyes && \
+HCDIR=`dirname $(readlink -f "$0")`
+
+dnf update --assumeyes
+
 dnf install --enablerepo=powertools --assumeyes \
 autoconf \
 bison \
@@ -20,25 +23,21 @@ wget \
 java-1.8.0-openjdk \
 java-1.8.0-openjdk-devel \
 python3 \
-screen && \
-dnf module --assumeyes install nodejs:14 && \
-npm install -g npm && \
-npm install -g cnpm --registry=https://registry.npm.taobao.org
-cd ~
-wget -O /usr/bin/ojcmp https://www.ssst.top/ojcmp-v0.4.0-x86_64-unknown-linux-gnu && \
-    chmod 755 /usr/bin/ojcmp
-cd ~
-git clone https://github.com.cnpmjs.org/flaryer/nsjail.git && cd nsjail && git checkout real_usr_time_kill && \
-    make && mv ./nsjail /usr/bin
-cd ~
-git clone https://github.com.cnpmjs.org/flaryer/Heng-Client.git && cd Heng-Client && git checkout only_nsjail && cnpm i && npm run build
-cd ~
-cp ~/Heng-Client/Tools/testlib.h /testlib.h && chown 1000:1000 /testlib.h
-cd ~
+nodejs:14
 
-cd ~
-wget https://download.redis.io/releases/redis-6.2.5.tar.gz && tar -zxf redis-6.2.5.tar.gz && cd redis-6.2.5 && make && make install
-cd ~
-git clone https://github.com.cnpmjs.org/flaryer/heng-controller.git && cd heng-controller && git checkout dev && cnpm i && npm run build
-cd ~
-git clone https://github.com.cnpmjs.org/flaryer/interval-front-end.git && cd interval-front-end && cnpm i && npm run generate
+npm update -g npm@latest
+
+curl https://sh.rustup.rs -sSf | sh -- --profile default -y
+
+source ~/.cargo/env
+source /etc/profile
+
+git clone -b v0.4.0 --depth=1 --single-branch https://github.com/ThinkSpiritLab/ojcmp.git ~/ojcmp
+cd ~/ojcmp && cargo build --release && cp target/release/ojcmp /usr/bin
+
+git clone -b real_usr_time_kill --depth=1 --single-branch https://github.com/flaryer/nsjail.git ~/nsjail
+cd ~/nsjail && make && mv ~/nsjail/nsjail /usr/bin
+
+cp $HCDIR/Tools/testlib.h /testlib.h
+
+cd $HCDIR && npm install && npm run build
