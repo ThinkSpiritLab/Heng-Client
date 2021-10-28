d8e412dd2d231523efb10d67bd594198bc80644e
flaeyer <a@oi.al>
2021-10-28 18:46:13 +0800

    faster crates-io

--- prepare-centos8.sh
+++ prepare-centos8.sh
@@ -36,7 +36,30 @@ dnf module --assumeyes install nodejs:14
 export RUSTUP_DIST_SERVER=https://mirror.sjtu.edu.cn/rust-static
 export RUSTUP_UPDATE_ROOT=https://mirror.sjtu.edu.cn/rust-static/rustup
 curl https://cdn.jsdelivr.net/gh/rust-lang-nursery/rustup.rs/rustup-init.sh -sSf | sh -s -- --profile default -y && source ~/.cargo/env
+echo '# 放到 `$HOME/.cargo/config` 文件中
+[source.crates-io]
+registry = "https://github.com/rust-lang/crates.io-index"
 
+# 替换成你偏好的镜像源
+replace-with = "sjtu"
+# replace-with = "ustc"
+
+# 清华大学
+[source.tuna]
+registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
+
+# 中国科学技术大学
+[source.ustc]
+registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+
+# 上海交通大学
+[source.sjtu]
+registry = "https://mirrors.sjtug.sjtu.edu.cn/git/crates.io-index"
+
+# rustcc 社区
+[source.rustcc]
+registry = "git://crates.rustcc.cn/crates.io-index"
+' > ~/.cargo/config
 source /etc/profile
 
 npm install -g npm --registry=https://registry.npm.taobao.org
